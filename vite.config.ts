1b7d172d349f23dcc7847e954b530334a007a81d
nikodev <martin.panebianco@smandes.gob.ar>
2023-11-16 12:44:38 -0300

    Add vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,39 +2,40 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import federation from "@originjs/vite-plugin-federation";
 
-// export default defineConfig({
+export default defineConfig({
+  // export default defineConfig(({ mode }) => {
+  //   const env = loadEnv("mock", process.cwd(), "");
+  //   const processEnvValues = {
+  //     "process.env": Object.entries(env).reduce((prev, [key, val]) => {
+  //       // console.log(key, val);
+  //       return {
+  //         ...prev,
+  //         [key]: val,
+  //       };
+  //     }, {}),
+  //   };
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv("mock", process.cwd(), "");
-  const processEnvValues = {
-    "process.env": Object.entries(env).reduce((prev, [key, val]) => {
-      // console.log(key, val);
-      return {
-        ...prev,
-        [key]: val,
-      };
-    }, {}),
-  };
+  //   return {
+  base: "./",
+  plugins: [
+    react(),
+    federation({
+      name: "Microfrontend-concept-shell",
+      remotes: {
+        // "mf-parcel": `${env.VITE_PARCEL_URL}`,
+        "mf-parcel":
+          "https://microfront-parcel-test.netlify.app/assets/parcel.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  // define: processEnvValues,
 
-  return {
-    base: "./",
-    plugins: [
-      react(),
-      federation({
-        name: "Microfrontend-concept-shell",
-        remotes: {
-          "mf-parcel": `${env.VITE_PARCEL_URL}`,
-        },
-        shared: ["react", "react-dom"],
-      }),
-    ],
-    define: processEnvValues,
-
-    build: {
-      modulePreload: false,
-      target: "esnext",
-      minify: false,
-      cssCodeSplit: false,
-    },
-  };
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+  // };
 });
