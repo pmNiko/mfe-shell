fd3e6d387198c836b00c85e7d125c75a417d572c
nikodev <martin.panebianco@smandes.gob.ar>
2023-11-16 11:38:14 -0300

    Config .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,34 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import federation from "@originjs/vite-plugin-federation";
 
-export default defineConfig({
-  base: "./",
-  plugins: [
-    react(),
-    federation({
-      name: "Microfrontend-concept-shell",
-      remotes: {
-        mfConceptRemoteAppTest:
-          // "http://staging.smandes.gov.ar/parcelTest/assets/remoteEntry.js",
-          "https://mf-parcel.onrender.com/assets/remoteEntry.js",
-        // "https://microfront-parcel-test.netlify.app/assets/remoteEntry.js",
-        // "http://localhost:5001/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom"],
-    }),
-  ],
+// export default defineConfig({
 
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
+export default defineConfig(({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+
+  return {
+    base: "./",
+    plugins: [
+      react(),
+      federation({
+        name: "Microfrontend-concept-shell",
+        remotes: {
+          mfConceptRemoteAppTest:
+            // "http://staging.smandes.gov.ar/parcelTest/assets/remoteEntry.js",
+            "http://localhost:5001/assets/parcel.js",
+          // "https://microfront-parcel-test.netlify.app/assets/remoteEntry.js",
+          // "http://localhost:5001/assets/remoteEntry.js",
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
