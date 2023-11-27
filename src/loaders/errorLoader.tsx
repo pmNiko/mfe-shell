4936cdd26807b35af5cf4933ff1d5bd56f50010b
nikodev <martin.panebianco@smandes.gob.ar>
2023-11-27 10:59:19 -0300

    Fix errorLoader

--- src/loaders/errorLoader.tsx
+++ src/loaders/errorLoader.tsx
@@ -1,5 +1,5 @@
 export const errorLoader = async () => {
-  return await new Promise((resolve, reject) => {
+  return await new Promise((__, reject) => {
     setTimeout(() => {
       reject(new Error("Error de carga"));
     }, 1000);
